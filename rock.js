3999b972cdcb91e6b0e70d416bd09632c1445097
Joschka César <cesar@posteo.de>
2020-02-21 08:07:52 +0100

    End of Game sequence added.

--- rock.js
+++ rock.js
@@ -16,6 +16,7 @@ function computerPlay() { //Generates a random number between 1 and 3 then retur
         }
     }
 function playRound(playerSelection, computerSelection) { // goes through all the cases and returns who won.
+    gameWinner.textContent = "";
     if (playerSelection == 1)  { // This is too bloated. Could be reworked with switch-statement, maybe?
         if (computerSelection == "SCISSORS") {
             playerScore++;
@@ -58,12 +59,21 @@ function playRound(playerSelection, computerSelection) { // goes through all the
     else {
         return "No valid input." 
     }
-score.textContent = "Player: " + playerScore + " - Computer: " + computerScore;
  }
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
         playRound(button.id, computerPlay());
+        if (playerScore == 5 || computerScore == 5) {
+            score.textContent = "Player: " + playerScore + " - Computer: " + computerScore;
+            gameWinner.textContent = "The game is over. Play again?"
+            playerScore = 0;
+            computerScore = 0;
+        }
+        else {
+            score.textContent = "Player: " + playerScore + " - Computer: " + computerScore;
+        }
+
       });
   });
